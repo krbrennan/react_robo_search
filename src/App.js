c71ddd6a9b64270c93c2ff02a4bddb4430c8848b
krbrennan <krb.brennan@gmail.com>
2018-07-02 17:34:00 -0400

    Added scroll feature to maintain view in page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import robots from './robots.js';
 import SearchBox from './SearchBox.js';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
   }
 
   onSearchChange = (event) => {
@@ -25,7 +32,9 @@ class App extends Component {
       <div className='tc'>
         <h1 className='f1'>RoboSearch</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
