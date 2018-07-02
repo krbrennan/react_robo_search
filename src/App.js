6afe562d87e187c80841a256abc79b2a054b992f
krbrennan <krb.brennan@gmail.com>
2018-07-02 16:46:34 -0400

    styling commits

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import robots from './robots.js';
 import SearchBox from './SearchBox.js';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -22,7 +23,7 @@ class App extends Component {
     });
     return (
       <div className='tc'>
-        <h1>RoboSearch</h1>
+        <h1 className='f1'>RoboSearch</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
