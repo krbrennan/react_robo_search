3d4e5d2c8a62f957538f382aafdac89dac5e2bab
krbrennan <krb.brennan@gmail.com>
2019-06-12 12:47:10 -0400

    initializing gh-pagaes

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
     });
     return (
       <div className='tc'>
-        <h1 className='f1'>RoboSearch</h1>
+        <h1 className='f1'>RobotSearch</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
